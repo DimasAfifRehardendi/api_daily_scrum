8d550f14b4e38e4ec6ee86b4568c134ddcaecf4d
DimasAfifRehardendi <dimas_afif_27rpl@student.smktelkom-mlg.sch.id>
2020-04-01 18:39:17 +0700

    penambahan api select all daily scum

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'UserController@login'); //do login
-Route::post('register', 'UserController@store'); //create petugas
+Route::post('register', 'UserController@store'); //create user
 
 
 Route::group(['middleware' => ['jwt.verify']], function () {
@@ -13,9 +13,9 @@ Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('user/{limit}/{offset}', "UserController@getAll"); //menampilkan all user
 	Route::post('logout', "UserController@logout"); //cek token
     
-    Route::get('daily/{id}', "DailyController@index"); //read poin
-    Route::get('daily/{limit}/{offset}', "DailyController@getAll"); //read poin
-	Route::post('daily', 'DailyController@store'); //create poin
-	Route::delete('daily/{id}', "DailyController@delete"); //delete poin
+    Route::get('daily/{id}', "DailyController@index"); //read daily
+    Route::get('daily/{limit}/{offset}', "DailyController@getAll"); //read daily
+	Route::post('daily', 'DailyController@store'); //create daily
+	Route::delete('daily/{id}', "DailyController@delete"); //delete daily
 
 });
